8087f28e28687b61b3614b2a57106c9bb224f19f
Te <tsai.te0210@gmail.com>
2023-08-06 00:58:34 -0400

    first commit

---
+++ src/test/java/com/springbootjenkinsapi/JenkinsDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springbootjenkinsapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class JenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("Test case executing...");
+		assert(true);
+	}
+
+}
